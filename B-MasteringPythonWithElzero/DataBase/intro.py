cb4dc5bef213f77edb37cfa306e5cf29e7604a1c
m0hvmed_ali <mohamedalix546@gmail.com>
2026-09-19 20:49:38 +0300

    ...

---
+++ B-MasteringPythonWithElzero/DataBase/intro.py
@@ -0,0 +1,19 @@
+# ╭───────────╮
+# │ [Dtabase] │
+# ╰───────────╯
+#Firstly we need to read a psql tutorial.
+""" 
+overview: 
+-Setup,
+-Work with psql, 
+-Create db, 
+-connection, 
+-Creat table[data type, constraints]
+-insert value
+-import data from csv use \copy table name(colum0,column1,...) 
+                        FROM 'file path'
+                        with(FORMAT file type, Header true/false)
+-insert data from mokaroo site
+-insert from sql file use \i file path
+-sort by order by
+"""
